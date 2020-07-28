715910185f46ebc57e1d90b8455f76a9f1caa7a0
Shashank Biplav <Biplavshashank7@gmail.com>
2020-07-29 02:02:42 +0530

    Added middleware to avoid CORS errors

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.listen(5500, ()=>{
     console.log('listening on port:5500');
 });
