36add3431241c5f18c12462359c1a9141ca32ec7
Shashank Biplav <Biplavshashank7@gmail.com>
2020-08-10 21:58:26 +0530

    Added ADMIN signup and login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,8 @@ const Chef = require('../models/chef');
 
 const User = require('../models/user');
 
+const Administrator = require('../models/administrator');
+
 exports.userSignup = async (req, res, next)=>{
     const errors = expressValidator.validationResult(req);
     if (!errors.isEmpty()) {
@@ -149,6 +151,81 @@ exports.cheflogin =async(req, res, next) => {
         res.status(200).json({
             token: token,
             userId: loadedChef._id.toString()
+        });
+    }
+    catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.administratorSignup = async (req, res, next) =>{
+    const errors = expressValidator.validationResult(req);
+    if (!errors.isEmpty()){
+        const error = new Error('Validation Failed');
+        error.statusCode = 422;
+        error.data = errors.array();
+        return next(error);
+    }
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const preExistingAdmin = await Administrator.findOne({email: email});
+    if (preExistingAdmin){
+        const error = new Error('Admin with this email already exists');
+        error.statusCode = 422;
+        return next(error);
+    }
+    try {
+        const hashedPwd = await bcrypt.hash(password, 12);
+        const admin = new Administrator({
+            email: email,
+            password: hashedPwd,
+            name: name
+        });
+        const result = await admin.save();
+        res.status(201).json({
+            message: 'Admin Created',
+            userId: result._id
+        });
+    }
+    catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    }
+};
+
+exports.administratorLogin = async (req, res, next) =>{
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedAdmin;
+    try {
+        const admin = await Administrator.findOne({email: email});
+        if (!admin){
+            const error = new Error('Admin with this email doesn\'t exist');
+            error.statusCode = 401;
+            throw error;
+        }
+        loadedAdmin = admin;
+        const isPwdEqual = await bcrypt.compare(password, admin.password);
+        if (!isPwdEqual){
+            const error = new Error('Wrong Password');
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = jwt.sign({
+            email: loadedAdmin.email,
+            userId: loadedAdmin._id.toString()
+        },'yoursuperdupersecretkeythatisknownonlytoyouandtheserver',{
+            expiresIn: '24h'
+        });
+        res.status(200).json({
+            token: token,
+            userId: loadedAdmin._id.toString()
         });
     }
     catch (err) {
--- routes/auth.js
+++ routes/auth.js
@@ -36,4 +36,16 @@ router.post('/chef/login',
     [expressValidator.check('email').isEmail().normalizeEmail()],
     authController.cheflogin);
 
+//ADMINISTRATOR SIGNUP   TODO: ALL ADMIN ROUTES AND CONTROLLERS
+router.post('/administrator/signup',[expressValidator.check('name').trim().not().isEmpty(),
+    expressValidator.check('email').isEmail().withMessage('Invalid Email').normalizeEmail(),
+    expressValidator.check('password').trim().isLength({
+        min: 10
+    })
+], authController.administratorSignup);
+
+//ADMINISTRATOR LOGIN
+router.post('/administrator/login', [expressValidator.check('email').isEmail().normalizeEmail()],
+   authController.administratorLogin );
+
 module.exports = router;
